1eafbfd6022768af7e5f195d441c74bc1095b5fd
axelwise <axelwise676@gmail.com>
2023-07-31 20:21:50 +0100

    changed folder name to better represent Route

--- src/app/components/weather.js
+++ src/app/components/weather.js
@@ -1,25 +1,17 @@
 'use client'
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 
 export function Weather() {
     const [city, setCity] = useState('');
-    const router = useRouter();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setCity('');
-        const res = await fetch(`/api/weather?city=${city}`)
+        const res = await fetch(`/api/location?city=${city}`)
         const data = await res.json();
         console.log(data);
     }
-
-    // async function handleClick() {
-    //     const res = await fetch(`/api/weather`);
-    //     const data = await res.json();
-    //     console.log(data);
-    // };
     return (
         <div>
             <form onSubmit={handleSubmit}>
